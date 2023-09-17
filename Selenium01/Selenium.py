5881ee7848f2f690824bdd3f6cc5ad3bbd53e78c
arkwadas <ark.wadas@gmail.com>
2023-09-17 23:43:16 +0200

    Dodanie kolejnych funkcji
    Znajdz, kliknij, wpisz, przejdz

--- Selenium01/Selenium.py
+++ Selenium01/Selenium.py
@@ -5,5 +5,25 @@ driver = webdriver.Chrome()
 driver.get('https:/google.com')
 
 time.sleep(1)
+print('strona :', driver.title)
+
+# button1_accept = driver.find_element(by= 'id', value= 'L2AGLb')
+button1_accept = driver.find_element(by= 'id', value= 'L2AGLb').click()
+time.sleep(1)
+
+textarea_googleFinder = driver.find_element(by= 'id', value= 'APjFqb')
+time.sleep(1)
+
+textarea_googleFinder.send_keys('Koduj z pythonem')
+time.sleep(1)
+
+search_button1 = driver.find_element(by= 'name', value= 'btnK')
+search_button1.submit()
+time.sleep(1)
+# print(button1_accept)
+# print(type)
+
+# button1_accept.click()
+# time.sleep(1)
 
 driver.quit()
